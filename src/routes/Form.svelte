483e09b2f9a9769ad7990e319848b2265bb44f18
Hinata Oishi <92960051+te19oishi@users.noreply.github.com>
2024-10-25 19:36:19 +0900

    Add Form component to +page.svelte

---
+++ src/routes/Form.svelte
@@ -0,0 +1,18 @@
+<script>
+  let name = '';
+  let email = '';
+
+  function handleSubmit() {
+    // Handle form submission
+  }
+</script>
+
+<form on:submit={handleSubmit}>
+  <label for="name">名前:</label>
+  <input type="text" id="name" bind:value={name} />
+
+  <label for="email">メールアドレス:</label>
+  <input type="email" id="email" bind:value={email} />
+
+  <button type="submit">送信</button>
+</form>
